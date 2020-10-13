531c985619cbac2c50e08f7902ab5835bc0f0da2
Héctor Sabillón <h.hsabillon@gmail.com>
2020-10-12 20:54:36 -0600

    Implementación del método que renderiza la vista de registro en Cashize

---
+++ controllers/usuarioController.js
@@ -0,0 +1,13 @@
+// Importar los módulos requeridos
+const mongoose = require("mongoose");
+const Usuario = mongoose.model("Usuarios");
+
+// Cargar el formulario de la creación de una cuenta de usuario
+exports.formularioCrearCuenta = (req, res, next) => {
+  res.render("registrarse", { layout: "auth" });
+};
+
+// Procesar el formulario de creación de cuenta
+exports.crearCuenta = (req, res, next) => {
+  // TODO: Obtener la información desde el req.body (body-parser)
+};
