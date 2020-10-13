fe7ec80d7e07e267e386727c93093b2eaf81847b
Héctor Sabillón <h.hsabillon@gmail.com>
2020-10-12 20:56:01 -0600

    Implementación del controlador de Usuario. Creación de la ruta get para registro de usuarios

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 // Importar los módulos requeridos
 const express = require("express");
+const usuarioController = require("../controllers/usuarioController");
 
 // Configura y mantiene todos los endpoints en el servidor
 const router = express.Router();
@@ -9,6 +10,8 @@ module.exports = () => {
   router.get("/", (req, res, next) => {
     res.send("¡Bienvenido a Cashize!");
   });
+
+  router.get("/crear-cuenta", usuarioController.formularioCrearCuenta);
 
   return router;
 };
