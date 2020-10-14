f342a32f5ac080c1b80f08b545b67f958681f22f
Héctor Sabillón <h.hsabillon@gmail.com>
2020-10-13 20:57:18 -0600

    Implementación de body-parser

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 require("./config/db");
 const exphbs = require("express-handlebars");
 const router = require("./routes/index");
+const bodyParser = require("body-parser");
 
 // Habilitar el archivo de variables de entorno
 require("dotenv").config({ path: ".env" });
@@ -14,6 +15,9 @@ const app = express();
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 
 app.set("view engine", "hbs");
+
+// Habilitar body-parser para obtener el cuerpo de la petición
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Implementar nuestro router
 app.use("/", router());
