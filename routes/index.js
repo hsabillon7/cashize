344daf00938d5a0784859be01a6b5422f227046d
Héctor Sabillón <h.hsabillon@gmail.com>
2020-10-22 20:55:32 -0600

    Implementación de express-validator para la ruta de creación de usuario

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const usuarioController = require("../controllers/usuarioController");
 const authController = require("../controllers/authController");
+const { check } = require("express-validator");
 
 // Configura y mantiene todos los endpoints en el servidor
 const router = express.Router();
@@ -15,11 +16,32 @@ module.exports = () => {
   // Rutas para usuario
   router.get("/crear-cuenta", usuarioController.formularioCrearCuenta);
 
-  router.post("/registrarse", usuarioController.crearCuenta);
+  router.post(
+    "/registrarse",
+    [
+      // Realizar una verificación de los atributos del formulario
+      // https://express-validator.github.io/docs/index.html
+      check("nombre", "Debes ingresar tu nombre completo.")
+        .not()
+        .isEmpty()
+        .escape(),
+      check("email", "Debes ingresar un correo electrónico.").not().isEmpty(),
+      check("email", "El correo electrónico ingresado no es válido.")
+        .isEmail()
+        .normalizeEmail(),
+      check("password", "Debes ingresar una contraseña").not().isEmpty(),
+    ],
+    usuarioController.crearCuenta
+  );
 
   router.get("/iniciar-sesion", usuarioController.formularioIniciarSesion);
 
   router.post("/iniciar-sesion", authController.autenticarUsuario);
+
+  // Rutas de administración
+  router.get("/administrar", (req, res, next) => {
+    res.send("Administración del sitio");
+  });
 
   return router;
 };
