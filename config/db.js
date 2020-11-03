58cfb24cf05ef50593c4f00d94ddf6b2dc22a5da
Héctor Sabillón <h.hsabillon@gmail.com>
2020-11-02 20:31:48 -0600

    Se agrega el modelo de producto para su implementación

--- config/db.js
+++ config/db.js
@@ -1,17 +1,18 @@
 // Módulos requeridos
 const mongoose = require("mongoose");
-require("dotenv").config({ path: ".env"});
+require("dotenv").config({ path: ".env" });
 
 // Configuración de Mongoose
 mongoose.connect(process.env.DATABASE, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 // Iniciar la conexión al servidor cloud mongo
-mongoose.connection.on("error", error => {
+mongoose.connection.on("error", (error) => {
   console.log(error);
 });
 
 // Importar los modelos requeridos
 require("../models/Usuario");
+require("../models/Producto");
